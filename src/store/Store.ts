7c3e9cf756cc45cc8ba191bc1a6f1ae69f668a78
Pramit Bhar <pramitbhar26@gmail.com>
2024-01-02 21:06:44 +0530

    commit store

---
+++ src/store/Store.ts
@@ -0,0 +1,28 @@
+export type UserId = string;
+export interface Chat{
+    id:string,
+    userId:UserId,
+    name:string,
+    message:string,
+    upvotes:UserId[];
+}
+
+export abstract class Store{
+    constructor(){
+
+    }
+    initRoom(roomId:string){
+
+    }
+    getChats(room:string, limit:number, offset:number){
+
+    }
+
+    addChats(userId:UserId,name:string,room:string,message:string){
+
+    }
+
+    upvote(userId:UserId,room:string,chatId: string){
+
+    }
+}
